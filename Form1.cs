1c683a06bcc1b0e149f50361d192c5fd31d855cd
AvgEnjoyer <93446740+AvgEnjoyer@users.noreply.github.com>
2021-10-31 13:50:27 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_1
+{
+    
+    public partial class Form1 : Form
+    {
+       private Grid GR = new Grid();
+       private _26BasedSystem sys26 = new _26BasedSystem();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            InitTable(GR.RowCount, GR.ColCount);
+        }
+        private void InitTable(int row,int col)
+        {
+            dataGridView1.AllowUserToAddRows = false;
+            for (int i = 0;i<col;i++)
+            {
+                string colname = sys26.To26Sys(i);
+                dataGridView1.Columns.Add(colname, colname);
+                dataGridView1.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+
+            dataGridView1.RowCount = row;
+            for (int i = 0; i < row; i++)
+                dataGridView1.Rows[i].HeaderCell.Value = i.ToString();
+            GR.SetGrid(row, col);
+
+            dataGridView1.AllowUserToAddRows = false;
+        }
+        
+
+      
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            int col = dataGridView1.SelectedCells[0].ColumnIndex;
+            int row = dataGridView1.SelectedCells[0].RowIndex;
+            string expr = ExprTextBox.Text;
+            if (expr == "")
+                return;
+            GR.ChangeCellWithAllPointers(row, col, expr, dataGridView1);
+            dataGridView1[col, row].Value = GR.grid[row][col].Value;
+
+        }
+        private void dataGridView1_Cell_Click(object sender,EventArgs e)
+        {
+            int col = dataGridView1.SelectedCells[0].ColumnIndex;
+            int row = dataGridView1.SelectedCells[0].RowIndex;
+
+            string expr = GR.grid[row][col].Expression;
+            string value = GR.grid[row][col].Value;
+
+            ExprTextBox.Text = expr;
+            ExprTextBox.Focus();
+        }
+
+        private void AddRow_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow row = new DataGridViewRow();
+            if(dataGridView1.Columns.Count==0)
+            {
+                MessageBox.Show("There no columns!");
+                return;
+            }
+            dataGridView1.Rows.Add(row);
+            RefreshRowNumbers();
+            GR.AddRow(dataGridView1);
+        }
+
+        private void RefreshRowNumbers()
+        {
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                dataGridView1.Rows[i].HeaderCell.Value = i.ToString();
+            }
+        }
+
+        private void DeleteRow_Click(object sender, EventArgs e)
+        {
+            int curRow = GR.RowCount - 1;
+            if (!GR.DeleteRow(dataGridView1))
+                return;
+
+            dataGridView1.Rows.RemoveAt(curRow);
+         //   RefreshRowNumbers();
+        }
+
+        private void AddColumn_Click(object sender, EventArgs e)
+        {
+            string colname = sys26.To26Sys(GR.ColCount);
+            dataGridView1.Columns.Add(colname, colname);
+            GR.AddCol(dataGridView1);
+        }
+
+        private void DeleteColumn_Click(object sender, EventArgs e)
+        {
+            int curCol = GR.ColCount - 1;
+            if (!GR.DeleteCol(dataGridView1))
+                return;
+            dataGridView1.Columns.RemoveAt(curCol);
+        }
+        private void SaveFile()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "GridFile|*.grd";
+            saveFileDialog.Title = "Save Grid File";
+            saveFileDialog.ShowDialog();
+            if (saveFileDialog.FileName != "")
+            {
+                FileStream fs = (FileStream)saveFileDialog.OpenFile();
+                StreamWriter sw = new StreamWriter(fs);
+                GR.Save(sw);
+                sw.Close();
+                fs.Close();
+            }
+        }
+        private void OpenFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "DGridFile|*.grd";
+            openFileDialog.Title = "Select Grid File";
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+                return;
+            StreamReader sr = new StreamReader(openFileDialog.FileName);
+            GR.Clear();
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            Int32.TryParse(sr.ReadLine(), out int row);
+            Int32.TryParse(sr.ReadLine(), out int col);
+            InitTable(row, col);
+            GR.Open(row, col, sr, dataGridView1);
+            sr.Close();
+
+        }
+
+       
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            OpenFile();
+        }
+    }
+}
---
+++ LabCalculatorVisitor.cs
@@ -0,0 +1,112 @@
+﻿using Antlr4.Runtime.Misc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace OOP_1
+{
+
+    class LabCalculatorVisitor : LabCalculatorBaseVisitor<double>
+    {
+        //таблиця ідентифікаторів (тут для прикладу)
+        //в лабораторній роботі заміните на свою!!!!
+        Dictionary<string, double> tableIdentifier = new Dictionary<string, double>();
+        public override double VisitCompileUnit(LabCalculatorParser.CompileUnitContext context)
+        {
+            return Visit(context.expression());
+        }
+        public override double VisitNumberExpr(LabCalculatorParser.NumberExprContext context)
+        {
+            var result = double.Parse(context.GetText());
+            Debug.WriteLine(result);
+            return result;
+        }
+        //IdentifierExpr
+        public override double VisitIdentifierExpr(LabCalculatorParser.IdentifierExprContext context)
+        {
+            var result = context.GetText();
+            double value;
+            //видобути значення змінної з таблиці
+            if (tableIdentifier.TryGetValue(result.ToString(), out value))
+            {
+                return value;
+            }
+            else
+            {
+                return 0.0;
+            }
+        }
+        public override double VisitPathenthesizedExpr(LabCalculatorParser.PathenthesizedExprContext context)
+        {
+            return Visit(context.expression());
+        }
+            
+        public override double VisitExponentialExpr(LabCalculatorParser.ExponentialExprContext context)
+        {
+            var left = WalkLeft(context);
+            var right = WalkRight(context);
+            Debug.WriteLine("{0}^{1}", left, right);
+            return System.Math.Pow(left, right);
+        }
+        public override double VisitAdditiveExpr(LabCalculatorParser.AdditiveExprContext context)
+        {
+            var left = WalkLeft(context);
+            var right = WalkRight(context);
+            if (context.operatorToken.Type == LabCalculatorLexer.ADD)
+            {
+                Debug.WriteLine("{0}+{1}", left, right);
+                return left + right;
+            }
+            else //LabCalculatorLexer.SUBTRACT
+            {
+                Debug.WriteLine("{0}-{1}", left, right);
+                return left - right;
+            }
+        }
+        public override double VisitMultiplicativeExpr(LabCalculatorParser.MultiplicativeExprContext context)
+        {
+            var left = WalkLeft(context);
+            var right = WalkRight(context);
+            if (context.operatorToken.Type == LabCalculatorLexer.MULTIPLY)
+            {
+                Debug.WriteLine("{0}*{1}", left, right);
+                return left * right;
+            }
+            else //LabCalculatorLexer.DIVIDE
+            {
+                Debug.WriteLine("{0},{1}", left, right);
+                return left / right;
+            }
+        }
+        public override double VisitMaxExpr( LabCalculatorParser.MaxExprContext context)
+        {
+            var left = WalkLeft(context);
+            var right = WalkRight(context);
+            Debug.WriteLine("max({0},{1})", left, right);
+            if (left > right)
+                return left;
+            else return right;
+        }
+        public override double VisitMinExpr([NotNull] LabCalculatorParser.MinExprContext context)
+        {
+            var left = WalkLeft(context);
+            var right = WalkRight(context);
+            Debug.WriteLine("min({0},{1})", left, right);
+            if (left > right)
+                return right;
+            else return left;
+        }
+        private double WalkLeft(LabCalculatorParser.ExpressionContext context)
+        {
+            return Visit(context.GetRuleContext<LabCalculatorParser.ExpressionContext>(0));
+        }
+        private double WalkRight(LabCalculatorParser.ExpressionContext context)
+        {
+            return Visit(context.GetRuleContext<LabCalculatorParser.ExpressionContext>(1));
+        }
+    }
+}
+
+
