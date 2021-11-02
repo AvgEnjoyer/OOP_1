d0cf0fe57f24cb9218993d80e53071093ea095bc
AvgEnjoyer <93446740+AvgEnjoyer@users.noreply.github.com>
2021-11-02 10:21:07 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace OOP_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ExprTextBox = new System.Windows.Forms.TextBox();
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.open = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AddRow = new System.Windows.Forms.Button();
+            this.DeleteRow = new System.Windows.Forms.Button();
+            this.AddColumn = new System.Windows.Forms.Button();
+            this.DeleteColumn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(1163, 518);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_Cell_Click);
+            
+            
+            // 
+            // ExprTextBox
+            // 
+            this.ExprTextBox.Location = new System.Drawing.Point(12, 12);
+            this.ExprTextBox.Name = "ExprTextBox";
+            this.ExprTextBox.Size = new System.Drawing.Size(407, 23);
+            this.ExprTextBox.TabIndex = 5;
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.Location = new System.Drawing.Point(425, 7);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(78, 32);
+            this.EnterButton.TabIndex = 6;
+            this.EnterButton.Text = "Enter";
+            this.EnterButton.UseVisualStyleBackColor = true;
+            this.EnterButton.Click += new System.EventHandler(this.EnterButton_Click);
+            // 
+            // open
+            // 
+            this.open.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.open.Location = new System.Drawing.Point(1002, 3);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(71, 41);
+            this.open.TabIndex = 7;
+            this.open.Text = "open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+
+            // 
+            // save
+            // 
+            this.save.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.save.Location = new System.Drawing.Point(1079, 1);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(78, 38);
+            this.save.TabIndex = 8;
+            this.save.Text = "save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(562, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Rows";
+            // 
+            // AddRow
+            // 
+            this.AddRow.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AddRow.Location = new System.Drawing.Point(628, 8);
+            this.AddRow.Name = "AddRow";
+            this.AddRow.Size = new System.Drawing.Size(41, 30);
+            this.AddRow.TabIndex = 10;
+            this.AddRow.Text = "+";
+            this.AddRow.UseVisualStyleBackColor = true;
+            this.AddRow.Click += new System.EventHandler(this.AddRow_Click);
+            // 
+            // DeleteRow
+            // 
+            this.DeleteRow.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DeleteRow.Location = new System.Drawing.Point(668, 7);
+            this.DeleteRow.Name = "DeleteRow";
+            this.DeleteRow.Size = new System.Drawing.Size(44, 30);
+            this.DeleteRow.TabIndex = 11;
+            this.DeleteRow.Text = "-";
+            this.DeleteRow.UseVisualStyleBackColor = true;
+            this.DeleteRow.Click += new System.EventHandler(this.DeleteRow_Click);
+            // 
+            // AddColumn
+            // 
+            this.AddColumn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AddColumn.Location = new System.Drawing.Point(799, 3);
+            this.AddColumn.Name = "AddColumn";
+            this.AddColumn.Size = new System.Drawing.Size(49, 36);
+            this.AddColumn.TabIndex = 12;
+            this.AddColumn.Text = "+";
+            this.AddColumn.UseVisualStyleBackColor = true;
+            this.AddColumn.Click += new System.EventHandler(this.AddColumn_Click);
+            // 
+            // DeleteColumn
+            // 
+            this.DeleteColumn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DeleteColumn.Location = new System.Drawing.Point(854, 3);
+            this.DeleteColumn.Name = "DeleteColumn";
+            this.DeleteColumn.Size = new System.Drawing.Size(46, 36);
+            this.DeleteColumn.TabIndex = 13;
+            this.DeleteColumn.Text = "-";
+            this.DeleteColumn.UseVisualStyleBackColor = true;
+            this.DeleteColumn.Click += new System.EventHandler(this.DeleteColumn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(728, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Columns";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1187, 798);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DeleteColumn);
+            this.Controls.Add(this.AddColumn);
+            this.Controls.Add(this.DeleteRow);
+            this.Controls.Add(this.AddRow);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.EnterButton);
+            this.Controls.Add(this.ExprTextBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Excel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox ExprTextBox;
+        private System.Windows.Forms.Button EnterButton;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button AddRow;
+        private System.Windows.Forms.Button DeleteRow;
+        private System.Windows.Forms.Button AddColumn;
+        private System.Windows.Forms.Button DeleteColumn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
